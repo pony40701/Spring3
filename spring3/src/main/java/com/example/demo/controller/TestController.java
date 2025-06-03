e7cc1b29c452cf45f6036d5388b82770e2b0e350
pony40701 <pony40701@gmail.com>
2025-06-03 11:06:10 +0800

    06031106

---
+++ spring3/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,61 @@
+package com.example.demo.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.model.MemberForm;
+import com.example.demo.model.User;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+	@GetMapping("/m1")
+	public String m1() {
+		return "page1";
+	}
+
+	@GetMapping("/dir1.page2")
+	public String m2() {
+		return "dir1/page2";
+	}
+
+	@GetMapping("/m3")
+	public String m3(Model model) {
+		model.addAttribute("companyName", "Brad Big Company");
+		model.addAttribute("companyTel", "04-12345678");
+
+		User user = new User();
+		user.setName("Brad");
+		user.setAge(18);
+		user.setGender(false);
+
+		model.addAttribute("user", user);
+
+		String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
+
+		model.addAttribute("now", now);
+
+		return "page3";
+	}
+
+	@GetMapping("/register")
+	public String m4(Model model) {
+		MemberForm form = new MemberForm();
+		form.setAccount("Account");
+		model.addAttribute("memberForm", form);
+
+		return "register";
+	}
+
+	@PostMapping("/register")
+	public String m5(Model model) {
+		return "";
+	}
+}
---
+++ spring3/src/main/java/com/example/demo/model/MemberForm.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+public class MemberForm {
+
+	private String account;
+	private String passwd;
+	private String cname;
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+}
